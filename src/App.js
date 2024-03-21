5c984952a192a5cef89c3672c0a795df59fdcc04
Naman Agarwal <agarwalnaman333@gmail.com>
2024-03-21 22:08:42 +0530

    Pushing code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ImageGenerator from "./components/ImageGenerator";
+import './App.css'
+const App = () => {
+  return (
+    <>  
+         <ImageGenerator/>
+     </> 
+  );
+}
+
+export default App;
